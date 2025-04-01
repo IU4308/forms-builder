feb7c9dda9516393f697b8a8388cf8a3a71c17b7
Ilya <ilya671qw@gmail.com>
2025-04-01 13:10:49 +0300

    finish header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,122 @@
-import { IoHomeOutline } from 'react-icons/io5';
 import { Button } from './ui/button';
 import { NavLink } from 'react-router';
 import { Input } from './ui/input';
 import { IoIosSearch } from 'react-icons/io';
 import ThemeSwitcher from './ThemeSwitcher';
+import { IoMenuOutline } from 'react-icons/io5';
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+type NavItemProps = {
+    title: string;
+    url: string;
+    variant?: string;
+};
+
+const menu = [
+    {
+        title: 'Home',
+        url: '/',
+    },
+    {
+        title: 'My Workspace',
+        url: '/workspace/1',
+    },
+    {
+        title: 'Admin',
+        url: '/admin',
+    },
+];
+
+const NavItem = ({ title, url }: NavItemProps) => {
+    return (
+        <NavLink to={url}>
+            <Button className="cursor-pointer" variant={'ghost'}>
+                {title}
+            </Button>
+        </NavLink>
+    );
+};
+
+const DesktopView = () => {
+    return (
+        <nav className="hidden md:flex max-w-[1280px] mx-auto w-full relative  py-2 px-4 gap-2 items-center justify-between">
+            <div className="flex shrink-0">
+                {menu.map((item) => (
+                    <NavItem {...item} />
+                ))}
+                <div className="px-2 relative w-full min-w-[400px] ">
+                    <IoIosSearch className="absolute right-4 top-1/4" />
+                    <Input type="text" className="" />
+                </div>
+            </div>
+            <div className=" flex gap-2">
+                <ThemeSwitcher />
+                <NavItem title="Login" url="/login" />
+            </div>
+        </nav>
+    );
+};
+
+const MobileView = () => {
+    return (
+        <nav className="px-2 md:hidden flex justify-between">
+            <div className="flex gap-2 shrink-0 w-[85%] border">
+                <DropDown />
+                {/* <NavItem title="Login" url="/login" /> */}
+                {/* {menu.map((item) => (
+                    <NavItem {...item} />
+                ))} */}
+                <div className="relative w-full">
+                    <IoIosSearch className="absolute right-4 top-1/4" />
+                    <Input type="text" className="" />
+                </div>
+            </div>
+            <div className=" flex gap-2">
+                <ThemeSwitcher />
+            </div>
+        </nav>
+    );
+};
+
+const DropDown = () => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <IoMenuOutline className="text-2xl" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
+                <DropdownMenuSeparator />
+                {menu.map((item) => (
+                    <DropdownMenuItem>
+                        <NavItem {...item} />
+                    </DropdownMenuItem>
+                ))}
+                <DropdownMenuItem>
+                    <NavItem title="Login" url="/login" />
+                </DropdownMenuItem>
+                {/* <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem> */}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
 
 export default function Header() {
     return (
-        <nav className="relative bg-secondary py-2 px-4 flex flex-col items-center sm:flex-row sm:justify-center">
-            <div className="absolute right-2">
-                <ThemeSwitcher />
-            </div>
-            <NavLink to={'/'}>
-                <Button className="cursor-pointer" variant={'ghost'}>
-                    <IoHomeOutline />
-                </Button>
-            </NavLink>
-            <NavLink to={'/'}>
-                <Button variant={'ghost'}>My Workspace</Button>
-            </NavLink>
-            <NavLink to={'/'}>
-                <Button variant={'ghost'}>Admin</Button>
-            </NavLink>
-            <div className="relative w-full max-w-[300px] sm:min-w-[300px]">
-                <IoIosSearch className="absolute right-2 top-1/4" />
-                <Input type="text" />
-            </div>
-        </nav>
+        <div className=" border-b">
+            <DesktopView />
+            <MobileView />
+        </div>
     );
 }
--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -5,7 +5,7 @@ import { useTheme } from '@/hooks/useTheme';
 export default function ThemeSwitcher() {
     const { toggleTheme } = useTheme();
     return (
-        <Button variant={'outline'} className="" onClick={toggleTheme}>
+        <Button variant={'ghost'} className="" onClick={toggleTheme}>
             <MdSunny className="text-2xl " />
         </Button>
     );
