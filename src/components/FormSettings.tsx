2e5a45a04ed9d0c1119845e06d1ac21e4f1ace17
Ilya <ilya671qw@gmail.com>
2025-04-07 18:19:53 +0300

    complete settings tab

--- src/components/FormSettings.tsx
+++ src/components/FormSettings.tsx
@@ -1,18 +1,47 @@
 import { Label } from '@radix-ui/react-label';
 import { Input } from './ui/input';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
+import { Button } from './ui/button';
 
 export default function FormSettings() {
     return (
-        <div className="flex flex-col bg-accent">
-            <div className=" py-4 px-6 flex flex-col gap-2">
+        <form className="flex flex-col gap-4  py-6 px-4 md:px-16">
+            <div className="flex flex-col gap-2">
                 <Label htmlFor="title">Title</Label>
-                <Input id="title" className="!bg-accent" />
+                <Input id="title" className="!bg-background" />
             </div>
-            <div className=" py-4 px-6 flex flex-col gap-2">
+            <div className="flex flex-col gap-2">
                 <Label htmlFor="description">Description</Label>
-                <Input id="description" className="!bg-accent" />
+                <Input id="description" className="!bg-background" />
             </div>
-            <div></div>
-        </div>
+            <div className="flex flex-col gap-4">
+                <Label>Topic</Label>
+                <Select>
+                    <SelectTrigger className="w-[180px] !bg-background">
+                        <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                    <SelectContent className="!bg-background">
+                        <SelectItem value="light">Light</SelectItem>
+                        <SelectItem value="dark">Dark</SelectItem>
+                        <SelectItem value="system">System</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+            <div className="flex flex-col gap-2">
+                <Label htmlFor="image">Image</Label>
+                <Input type="file" id="image" className="!bg-background" />
+            </div>
+            <div className="flex flex-col gap-2">
+                <Label htmlFor="tags">Tags</Label>
+                <Input id="tags" className="!bg-background" />
+            </div>
+            <Button className="my-4 ">Save</Button>
+        </form>
     );
 }
--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -1,11 +1,33 @@
 import CustomForm from '@/components/CustomForm';
 import FormSettings from '@/components/FormSettings';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { useState } from 'react';
 
 export default function Template() {
+    const [tabId, setTabId] = useState(1);
     return (
         <div className="max-w-[768px] mx-auto">
-            <FormSettings />
-            <CustomForm />
+            <div className="mb-4 flex gap-2 justify-center">
+                <Button
+                    variant={!tabId ? 'default' : 'ghost'}
+                    onClick={() => setTabId(0)}
+                >
+                    Questions
+                </Button>
+                <Button
+                    variant={tabId ? 'default' : 'ghost'}
+                    onClick={() => setTabId(1)}
+                >
+                    Settings
+                </Button>
+            </div>
+            <div className={cn('visible', tabId !== 0 && 'hidden')}>
+                <CustomForm />
+            </div>
+            <div className={cn('visible', tabId !== 1 && 'hidden')}>
+                <FormSettings />
+            </div>
         </div>
     );
 }
