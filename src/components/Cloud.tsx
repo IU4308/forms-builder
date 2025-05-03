98e54bf68b469d18a6f7b490159a33cc688e7e63
Ilya <ilya671qw@gmail.com>
2025-05-03 11:47:09 +0300

    style active tag in the tagcloud

--- src/components/Cloud.tsx
+++ src/components/Cloud.tsx
@@ -1,6 +1,7 @@
 import { TagCloud } from 'react-tagcloud';
 import { useLoaderData } from 'react-router';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 
 type LoaderData = {
     tags: {
@@ -19,6 +20,23 @@ export default function Cloud({
     const { t } = useTranslation();
     const { tags } = useLoaderData<LoaderData>();
 
+    const [activeId, setActiveId] = useState<number | null>(null);
+
+    const customRenderer = (tag: any, size: number) => {
+        const isActive = tag.id === activeId;
+
+        return (
+            <span
+                key={tag.id}
+                className={`mx-2 my-1 inline-block cursor-pointer transition 
+              ${isActive ? 'text-chart-2 font-bold underline' : 'text-foreground hover:text-chart-2 hover:opacity-80'}`}
+                style={{ fontSize: `${size}px` }}
+            >
+                {tag.value}
+            </span>
+        );
+    };
+
     return (
         <div className="py-4">
             <h1 className="mb-2">{t('home.tags')}</h1>
@@ -31,9 +49,12 @@ export default function Cloud({
                     id: tag.id,
                     templateIds: tag.templateIds,
                 }))}
+                renderer={customRenderer}
                 onClick={(tag) => {
                     // @ts-ignore
                     handleSelect(tag.templateIds);
+                    // @ts-ignore
+                    setActiveId(tag.id);
                 }}
                 className="cursor-pointer"
             />
